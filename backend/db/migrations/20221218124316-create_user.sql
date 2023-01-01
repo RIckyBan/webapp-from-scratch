d05720c3c0053cace6ffe1883df7813ce8228de8
Riki Bando <rickyban1231@gmail.com>
2023-01-01 20:26:12 +0900

    make db dir

--- backend/db/migrations/20221218124316-create_user.sql
+++ backend/db/migrations/20221218124316-create_user.sql
@@ -1,4 +1,12 @@
 
 -- +migrate Up
+CREATE TABLE users (
+  id VARCHAR(26) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL
+);
 
 -- +migrate Down
+DROP TABLE users;
