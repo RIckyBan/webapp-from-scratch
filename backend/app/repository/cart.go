646842a6725c5bc2ac7d837e0396b83e081549f0
Riki Bando <rickyban1231@gmail.com>
2023-02-12 10:14:46 +0900

    tiny fix

--- backend/app/repository/cart.go
+++ backend/app/repository/cart.go
@@ -9,5 +9,5 @@ import (
 
 type CartRepository interface {
 	GetAllItems(ctx context.Context, userID ulid.ULID) ([]*entity.Item, error)
-	AddItem(ctx context.Context, userID ulid.ULID, itemID ulid.ULID, quantity int) error
+	AddItem(ctx context.Context, userID, itemID ulid.ULID, quantity int) error
 }
